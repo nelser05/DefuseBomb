b564634282676d37dea2f3a58ff9f2f5bcedc4a0
Erik Nelson <nelser05@luther.edu>
2017-11-04 17:11:12 -0500

    Some small color and text changes

--- DefuseBomb/DefuseBomb.Android/MainActivity.cs
+++ DefuseBomb/DefuseBomb.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace DefuseBomb.Droid
 {
-    [Activity(Label = "DefuseBomb", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Boop", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
